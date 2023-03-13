83d246fd86295b4f708ebf5779d5e62b612b0447
cihaderoll <cihaderoll@gmail.com>
2023-03-13 23:03:34 +0300

    xunit first demo

--- XUnitTestDemo/UnitTest1.cs
+++ XUnitTestDemo/UnitTest1.cs
@@ -2,10 +2,29 @@ namespace XUnitTestDemo
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        public void PassingTest(int value)
         {
+            Assert.True(IsOdd(value));
+        }
 
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(4, Add(3, 2));
+        }
+
+        int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        bool IsOdd(int val)
+        {
+            return val % 2 == 1;
         }
     }
 }
