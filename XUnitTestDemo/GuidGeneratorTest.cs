b1dea16d3b94e62fda05a5ed28b68c39753b82f8
cihaderoll <cihaderoll@gmail.com>
2023-03-16 23:23:11 +0300

    IClassFixture and disabling parallel testing

---
+++ XUnitTestDemo/GuidGeneratorTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TargetProject;
+using Xunit.Abstractions;
+
+namespace XUnitTestDemo
+{
+    public class GuidGeneratorTest : IClassFixture<GuidGenerator>, IDisposable
+    {
+        public GuidGenerator _guidGenerator { get; set; }
+        public ITestOutputHelper _outputHelper{ get; set; }
+
+        public GuidGeneratorTest(ITestOutputHelper helper, GuidGenerator generator)
+        {
+            _guidGenerator = generator;
+            _outputHelper = helper;
+        }
+
+        [Fact]
+        public void GuidTestOne()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _outputHelper.WriteLine($"new guid -> {guid}");
+
+        }
+
+        [Fact]
+        public void GuidTestTwo()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _outputHelper.WriteLine($"new guid -> {guid}");
+        }
+
+        public void Dispose()
+        {
+            _outputHelper.WriteLine($"guid class disposed");
+        }
+    }
+}
