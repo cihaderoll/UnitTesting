25b0fd21fcb6d0d5c4f3b9cb6cc62694aa632c8e
cihaderoll <cihaderoll@gmail.com>
2023-03-16 19:48:50 +0300

    learning ClassData attribute

--- TargetProject/Calculator.cs
+++ TargetProject/Calculator.cs
@@ -8,9 +8,32 @@ namespace TargetProject
 {
     public class Calculator
     {
-        public int Add(int x, int y)
+        public int Value { get; private set; }
+
+        public int Add(int val)
         {
-            return x + y;
+            return Value += val;
+        }
+
+        public int Subtract(int val)
+        {
+            return Value -= val;
+        }
+
+        public int Multiply(int val)
+        {
+            if (Value == 0)
+                return Value = val;
+
+            return Value *= val;
+        }
+
+        public int Divide(int val)
+        {
+            if (Value == 0)
+                return Value = val;
+
+            return Value /= val;
         }
     }
 }
--- XUnitTestDemo/CalculatorTest.cs
+++ XUnitTestDemo/CalculatorTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -20,22 +21,54 @@ namespace XUnitTestDemo
         [Fact]
         public void TestAdd()
         {
-            Assert.Equal(2, _sut.Add(3, 3));
+            Assert.Equal(2, _sut.Add(3));
         }
 
         [Theory]
         [MemberData(nameof(ProvideTestData))]
         public void TestAddWithManyData(int expected, params int[] valuesToAdd)
         {
-            Assert.Equal(expected, _sut.Add(valuesToAdd[0], valuesToAdd[1]));
+            foreach (var value in valuesToAdd)
+            {
+                _sut.Add(value);
+            }
+
+            Assert.Equal(expected, _sut.Value);
+        }
+
+        [Theory]
+        [ClassData(typeof(DivideTestData))]
+        public void TestDivideWithManyData(int expected, params int[] valuesToAdd)
+        {
+            foreach (var value in valuesToAdd)
+            {
+                _sut.Divide(value);
+            }
+
+            Assert.Equal(expected, _sut.Value);
         }
 
         public static IEnumerable<object[]> ProvideTestData()
         {
             yield return new object[] { 30, new int[] { 10, 20 } };
-            yield return new object[] { 12, new int[] { 3, 67 } };
+            yield return new object[] { 70, new int[] { 3, 67 } };
             yield return new object[] { 25, new int[] { 12, 13 } };
-            yield return new object[] { 15, new int[] { 40, -25 } };
+            yield return new object[] { 53, new int[] { 40, -25, 8, 6, 24 } };
+        }
+    }
+
+    public class DivideTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 2, new int[] { 50, 25 } };
+            yield return new object[] { 5, new int[] { 15, 3 } };
+            yield return new object[] { 45, new int[] { 15, 35 } };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
